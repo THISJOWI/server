f5e904021d1c51b41d78db89497cd9625c5a9d5e
THISJowi <thsjowi@gmail.com>
2025-12-05 18:18:57 +0100

    Fixed auth error

--- Authentication/src/main/java/uk/thisjowi/Authentication/config/SecurityConfig.java
+++ Authentication/src/main/java/uk/thisjowi/Authentication/config/SecurityConfig.java
@@ -39,6 +39,8 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 // Public-specific routes
                 .requestMatchers(
+                    "/api/v1/auth/register",
+                    "/api/v1/auth/login",
                     "/api/auth/register",
                     "/api/auth/login",
                     "/register",
